609b58c2002cf505f93df8b186ceed15a045ffa3
bily66 <97169724+bily66@users.noreply.github.com>
2022-08-03 23:11:27 +0800

    入文案、選單

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,16 @@ let height = $(window).height();
 
 AOS.init();
 
+$('.hamburger').on('click', function() {
+  $(this).toggleClass('opened');
+  $('nav').slideToggle();
+})
+
+$('nav a').on('click', function() {
+  $('.hamburger').removeClass('opened');
+  $('nav').slideUp();
+})
+
 $('.autoplay').slick({
   slidesToShow: 1,
   slidesToScroll: 1,
@@ -12,8 +22,29 @@ $('.autoplay').slick({
 });
 
 $('.slider-set').slick({
-  slidesToShow: 1,
   slidesToScroll: 1,
+  slidesToShow: 1,
+  asNavFor: '.slider-set-thumbnails',
+});
+
+$('.slider-set-thumbnails').slick({
+  slidesToScroll: 1,
+  slidesToShow: 1,
+  asNavFor: '.slider-set',
+  arrows: false
+});
+
+// Remove active class from all thumbnail slides
+$('.slider-set-thumbnails .slick-slide').removeClass('slick-active');
+
+// Set active class to first thumbnail slides
+$('.slider-set-thumbnails .slick-slide').eq(0).addClass('slick-active');
+
+// On before slide change match active thumbnail to current slide
+$('.slider-set').on('beforeChange', function (event, slick, currentSlide, nextSlide) {
+  var mySlideNumber = nextSlide;
+  $('.slider-set-thumbnails .slick-slide').removeClass('slick-active');
+  $('.slider-set-thumbnails .slick-slide').eq(mySlideNumber).addClass('slick-active');
 });
 
 $(window).on('resize', function() {
